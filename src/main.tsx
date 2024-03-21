5dc18d9fa17ff19a8270fea69dfa6e95ebea1a68
以我之名 <949119615@qq.com>
2024-03-21 17:54:33 +0800

    test

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 // this manifest is used temporarily for development purposes
-const manifestUrl = 'https://github.com/forbename/tongirls/tonconnect-manifest.json';
+const manifestUrl = 'https://forbename.github.io/tongirls/tonconnect-manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
